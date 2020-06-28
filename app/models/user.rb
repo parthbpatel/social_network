8ea8b270afc3749352192481068b723f89b5b586
parthbpatel <parthtech3010@gmail.com>
2020-06-28 14:48:37 +0530

    Add image field to registration page, fix syntax, change relevent link path.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and
   # :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable,
-         :validatable, omniauthable, omniauth_providers: %i[facebook]
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[facebook]
   validate :picture_size
 
   private
