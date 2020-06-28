d683d09e2f7b60535ac3e293ff2d4846d1cdfb1a
parthbpatel <parthtech3010@gmail.com>
2020-06-28 14:16:27 +0530

    Add mount_uploader to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
+  mount_uploader :image, ImageUploader
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :confirmable, :lockable, :timeoutable, :trackable and
+  # :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable,
+         :validatable, omniauthable, omniauth_providers: %i[facebook]
+  validate :picture_size
+
+  private
+  # Validates the size of an uploaded picture.
+  def picture_size
+    errors.add(:image, 'should be less than 1MB') if image.size > 1.megabytes
+  end
 end
