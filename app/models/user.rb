0c146f1926361eed109538cd56812537215d128c
parthbpatel <parthtech3010@gmail.com>
2020-07-04 15:16:34 +0530

    add method to return friends post and users own posts.

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,28 @@ class User < ApplicationRecord
 
   has_many :notifications, dependent: :destroy
 
+  # Returns a string containing this user's first name and last name
+  def full_name
+    "#{fname} #{lname}"
+  end
+
+  # Returns all posts from this user's friends and self
+  def friends_and_own_posts
+    myfriends = friends
+    our_posts = []
+    myfriends.each do |f|
+      f.posts.each do |p|
+        our_posts << p
+      end
+    end
+    posts.each do |p|
+      our_posts << p
+    end
+    our_posts
+  end
+
   private
+
   # Validates the size of an uploaded picture.
   def picture_size
     errors.add(:image, 'should be less than 1MB') if image.size > 1.megabytes
