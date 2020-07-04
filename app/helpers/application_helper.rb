87ae076a55c9b06e3a85753a44002323cf08e4fe
parthbpatel <parthtech3010@gmail.com>
2020-07-04 19:08:14 +0530

    add create method in likes controller and other private method that used in create method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
   # Returns the new record created in notifications table
   def new_notification(user, notice_id, notice_type)
     notice = user.notifications.build(notice_id: notice_id,
-    notice_type: notice_type)
+                                      notice_type: notice_type)
     user.notice_seen = false
     user.save
     notice
@@ -16,7 +18,23 @@ module ApplicationHelper
     return Post.find(notice.notice_id) if type == 'comment'
     return Post.find(notice.notice_id) if type == 'like-post'
     return unless type == 'like-comment'
+
     comment = Comment.find(notice.notice_id)
     Post.find(comment.post_id)
+  end
+
+  # Checks whether a post or comment has already been liked by the
+  # current user returning either true or false
+  def liked?(subject, type)
+    result = false
+    if type == 'post'
+      result = Like.where(user_id: current_user.id, post_id:
+                          subject.id).exists?
+    end
+    if type == 'comment'
+      result = Like.where(user_id: current_user.id, comment_id:
+                          subject.id).exists?
+    end
+    result
   end
 end
