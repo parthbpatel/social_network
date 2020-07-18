68496e801f143c7e14879bea6bc93238f537687c
parthbpatel <parthtech3010@gmail.com>
2020-07-18 16:08:19 +0530

    model changes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,35 +1,24 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :friend_sent, class_name: 'Friendship', foreign_key: 'sent_by_id', inverse_of: 'sent_by', dependent: :destroy
+  has_many :friend_request, class_name: 'Friendship', foreign_key: 'sent_to_id',
+                            inverse_of: 'sent_to', dependent: :destroy
+  has_many :friends, -> { merge(Friendship.friends) }, through: :friend_sent, source: :sent_to
+  has_many :pending_requests, -> { merge(Friendship.not_friends) }, through: :friend_sent, source: :sent_to
+  has_many :recieved_requests, -> { merge(Friendship.not_friends) }, through: :friend_request, source: :sent_by
+  has_many :notifications, dependent: :destroy
   mount_uploader :image, ImageUploader
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and
-  # :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[facebook]
+  validates :fname, length: { in: 3..15 }, presence: true
+  validates :lname, length: { in: 3..15 }, presence: true
   validate :picture_size
 
-  has_many :posts
-  has_many :comments
-  has_many :likes, dependent: :destroy
-
-  has_many :friend_sent, class_name: 'Friendship', foreign_key: 'sent_by_id',
-                        inverse_of: 'sent_by', dependent: :destroy
-
-  has_many :friend_request, class_name: 'Friendship', foreign_key: 'sent_to_id',
-                          inverse_of: 'sent_to', dependent: :destroy
-
-  has_many :friends, -> { merge(Friendship.friends) },
-           through: :friend_sent, source: :sent_to
-
-  has_many :pending_requests, -> { merge(Friendship.not_friend) },
-           through: :friend_sent, source: :sent_to
-
-  has_many :received_requests, -> { merge(Friendship.not_friend) },
-           through: :friend_request, source: :sent_by
-
-  has_many :notifications, dependent: :destroy
-
-  # Returns a string containing this user's first name and last name
   def full_name
     "#{fname} #{lname}"
   end
@@ -43,9 +32,11 @@ class User < ApplicationRecord
         our_posts << p
       end
     end
+
     posts.each do |p|
       our_posts << p
     end
+
     our_posts
   end
 
